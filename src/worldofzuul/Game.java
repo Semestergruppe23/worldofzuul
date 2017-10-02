84960af1313783b63e34ccb6201166d7af25f22e
mofrains <skakkristiansen@gmail.com>
2017-10-02 13:34:26 +0200

    Update Game.java
    Added comments, and added a single room

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -6,33 +6,34 @@ package worldofzuul;
  */
 public class Game 
 {
-    
-    private Parser parser;
-    private Room currentRoom;
+    private Parser parser;  //Creates an instance of the Parser class, and assigns it to the variable "parser"
+    private Room currentRoom;  // Creates an instance of the Room class, and assigns it to the variable "currentRoom"
         
-    public Game() 
+    public Game() //No-args constructor of the Game Class
     {
-        createRooms();
-        parser = new Parser();
+        createRooms();  //Kører createRooms klassen, som laver rummene i starten af spillet
+        parser = new Parser();  //Creates an instant of the parser class (continued from above for unknown reasons)
+								//, to continuelly run in the play method untill the game ends
     }
-    //yooo
- 
-//doooo222
-    private void createRooms()
+
+    private void createRooms()  //Is used in the game class, and creates the rooms of the game at start, with instances of the Room class
     {
-        Room outside, theatre, pub, lab, office;
+        Room outside, theatre, pub, lab, office, HUT; //Declares the rooms of the game, SLET HUT
       
-        outside = new Room("outside the main entrance of the university");
+        outside = new Room("outside the main entrance of the university"); // Uses the method Room, which declares instances of the Room class, with string arguments for each room
         theatre = new Room("in a lecture theatre");
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
+        HUT = new Room("In the HUT \n Exits: go east"); // SLET
         
-        outside.setExit("east", theatre);
+        outside.setExit("east", theatre);  //Uses the "sub-method" setExit in the Room class, to set which ways you can leave the rooms
         outside.setExit("south", lab);
         outside.setExit("west", pub);
 
         theatre.setExit("west", outside);
+        theatre.setExit("east", HUT); // SLET
+        HUT.setExit("east", theatre); 
 
         pub.setExit("east", outside);
 
@@ -41,23 +42,24 @@ public class Game
 
         office.setExit("west", lab);
 
-        currentRoom = outside;
+        currentRoom = outside;  // Sets the initial room to "outside" which is changed in the goRoom method below
     }
 
-    public void play() 
+    public void play() //Method to start the game
     {            
-        printWelcome();
+        printWelcome();  //Prints the welcome method, using the printWelcome method below
 
-        boolean finished = false;
-        while (! finished) {
+        boolean finished = false;  // Remains false until changed in the quit method below
+        while (! finished) {		//While the game is not finished, continuelly get commands from the parser.getCommand() Method
             Command command = parser.getCommand();
-            finished = processCommand(command);
+            finished = processCommand(command);		//Changes to "true" when the parser receives the word "QUIT"
         }
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    private void printWelcome()
-    {
+    private void printWelcome()  //Prints the welcome + the CommandWord.HELP (HASHMAP THING),
+	{			//which is "help", and the getLongDescription method for the current room, from the Room class
+    
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
@@ -66,30 +68,30 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
-    private boolean processCommand(Command command) 
+    private boolean processCommand(Command command)   //Processes a command object from the Command class
     {
         boolean wantToQuit = false;
 
-        CommandWord commandWord = command.getCommandWord();
+        CommandWord commandWord = command.getCommandWord();     //Creates a object from the getCommandWord() method in the Commandword class
 
-        if(commandWord == CommandWord.UNKNOWN) {
+        if(commandWord == CommandWord.UNKNOWN) {                //HASHMAP_THING, if the command != Go, quit, etc. sout "I dont know what... " 
             System.out.println("I don't know what you mean...");
             return false;
         }
 
-        if (commandWord == CommandWord.HELP) {
+        if (commandWord == CommandWord.HELP) {                  //If the commandWord is "help" call the printHelp() method below
             printHelp();
         }
-        else if (commandWord == CommandWord.GO) {
+        else if (commandWord == CommandWord.GO) {               //If the command is "go" call the goRoom method below
             goRoom(command);
         }
-        else if (commandWord == CommandWord.QUIT) {
+        else if (commandWord == CommandWord.QUIT) {             //If the command word is "quit" call the quit method below, with the argument "quit"
             wantToQuit = quit(command);
         }
         return wantToQuit;
     }
 
-    private void printHelp() 
+    private void printHelp()                                    //Prints a little text and calls the showCommands() method from the parser class
     {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
@@ -98,34 +100,34 @@ public class Game
         parser.showCommands();
     }
 
-    private void goRoom(Command command) 
+    private void goRoom(Command command)       //The method to go to another room                 
     {
-        if(!command.hasSecondWord()) {
+        if(!command.hasSecondWord()) {          //If the command does not have a second word, "Go where?" is asked
             System.out.println("Go where?");
             return;
         }
 
         String direction = command.getSecondWord();
 
-        Room nextRoom = currentRoom.getExit(direction);
+        Room nextRoom = currentRoom.getExit(direction);         //Makes the nextRoom from the Room class be the exit from the current room
 
         if (nextRoom == null) {
-            System.out.println("There is no door!");
+            System.out.println("There is no door!");            //Is nextRoom doesent have the wanted exit, print "There is no door"
         }
         else {
             currentRoom = nextRoom;
-            System.out.println(currentRoom.getLongDescription());
+            System.out.println(currentRoom.getLongDescription());   //Changes the currentRoom to the requested nextRoom, and prints the rooms description
         }
     }
 
-    private boolean quit(Command command) 
+    private boolean quit(Command command)       //Method to quit the game
     {
-        if(command.hasSecondWord()) {
+        if(command.hasSecondWord()) {           //If there is a word after "quit", returns false and asks "Quit what?"
             System.out.println("Quit what?");
             return false;
         }
         else {
-            return true;
+            return true;                        //Returns true if the command.equals("quit") and quits the game 
         }
     }
 }
