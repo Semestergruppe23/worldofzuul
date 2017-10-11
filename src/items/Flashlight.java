f6268e61bfacd54592a3839078e31f59fb171c0a
kristiant12 <32262503+kristiant12@users.noreply.github.com>
2017-10-11 15:13:15 +0200

    Ændring af navn

--- src/items/Flashlight.java
+++ src/items/Flashlight.java
@@ -2,13 +2,13 @@
 package items;
 // @author niemeijeren
  
-public class Flashligt {
+public class Flashlight {
     
     private static int volume = 10;
     private String canPickUp = "You can pick this up";
     private boolean hasBatteries = false;
             
-    public Flashligt() {
+    public Flashlight() {
         this.canPickUp = canPickUp;
         this.hasBatteries = hasBatteries;
         this.volume = volume;
